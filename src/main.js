233404e37b05cd26e0b5a4efb9414addab6fbdd6
Christian Torp <torp@Christians-Air.home>
2023-11-01 11:38:58 +0100

    updates

--- src/main.js
+++ src/main.js
@@ -1,115 +1,121 @@
-import { Telegraf, Markup } from 'telegraf'
-import { message } from 'telegraf/filters'
-import { generateGPT } from './openai.js'
-import config from 'config'
-
+import { Telegraf, Markup } from 'telegraf';
+import { message } from 'telegraf/filters';
+import { generateGPT } from './openai.js';
+import config from 'config';
 
 const bot = new Telegraf(config.get('APIKeys.TELEGRAM_TOKEN'), {
-    handlerTimeout: Infinity
-})
+  handlerTimeout: Infinity,
+});
 
-// Start command handler with a welcome message and command instructions in creating SEO-optimised meta description for blog posts. Provide me with keywords for your meta caption, and I'll save the results directly to your Notion page
 bot.start((ctx) => {
-    const welcomeMessage = "👋 Hey!\n\n" +
-        "I'm here to assist you in writing greeting messages for any occasion. Please tell me who is it for, and what is the occasion for the greeting.\n";
-    ctx.reply(welcomeMessage);
+  const welcomeMessage =
+    '👋 Hey!\n\n' +
+    "I'm here to assist you in writing greeting messages for any occasion. Please tell me who is it for, and what is the occasion for the greeting.\n";
+  ctx.reply(welcomeMessage);
 });
 bot.help((ctx) => {
-    sendCommandList(ctx);
+  sendCommandList(ctx);
 });
 
-bot.on(message('text'), processAIResponse)
-
-
-
+bot.on(message('text'), processAIResponse);
 
 function sendCommandList(ctx) {
-    const commandList = [
-        "/start - Press start and then provide me with some keywords. Who do you want to send the greeting to, and what is the occasion for the greeting",
-        "/help - Get help and instructions",
-    ];
+  const commandList = [
+    '/start - Press start and then provide me with some keywords. Who do you want to send the greeting to, and what is the occasion for the greeting',
+    '/help - Get help and instructions',
+  ];
 
-    const commandListMessage = "🆘 Available commands:\n" + commandList.join("\n");
-    ctx.reply(commandListMessage);
+  const commandListMessage =
+    '🆘 Available commands:\n' + commandList.join('\n');
+  ctx.reply(commandListMessage);
 }
-
 
 bot.action('send_to_notion', async (ctx) => {
-    const loginUrl = 'https://www.notion.so/login';
-    ctx.replyWithHTML('Login to your notion', Markup.inlineKeyboard([
-        Markup.button.callback('login to notion', loginUrl)]))
+  const loginUrl = 'https://www.notion.so/login';
+  ctx.replyWithHTML(
+    'Login to your notion',
+    Markup.inlineKeyboard([
+      Markup.button.callback('login to notion', loginUrl),
+    ]),
+  );
 });
 
-// Define a variable to store the original user input (keywords) and also to store the button click count in bot memory to prevent spam
+// Store the original user keywords and also to store the button click count in bot memory to prevent spam
 const originalUserInput = {};
-
-async function processAIResponse(ctx) {
-    try {
-        const text = ctx.message.text;
-        const minInputLength = 3;
-        const trimmedText = text.trim();
-
-        // Store the original user input
-        originalUserInput[ctx.from.id] = trimmedText;
-
-        console.log(`User input: ${trimmedText}`);
-
-        if (trimmedText.length < minInputLength) {
-            ctx.reply('Please provide a meaningful input with at least one keyword.');
-            return;
-        }
-
-        const response = await generateGPT(trimmedText);
-
-        if (!response) {
-            ctx.reply("Sorry, I couldn't generate content for you. Please try again.");
-        }
-        else {
-            const formattedResponse = `<b style="color:green;">Voila 🪄 Your greeting is ready!</b>\n\n${response.content}`;
-            ctx.replyWithHTML(formattedResponse, Markup.inlineKeyboard([
-                Markup.button.callback("👎 I don't like it. Give me another one!", 'generate_new')
-            ]));
-        }
-    } catch (error) {
-        console.log(`Error with OpenAI processing:`, error.message);
-    }
-}
-
 const buttonClickCount = {};
 
+async function processAIResponse(ctx) {
+  try {
+    const text = ctx.message.text;
+    const minInputLength = 3;
+    const trimmedText = text.trim();
+
+    originalUserInput[ctx.from.id] = trimmedText;
+
+    if (trimmedText.length < minInputLength) {
+      ctx.reply('Please provide a meaningful input with at least one keyword.');
+      return;
+    }
+
+    const response = await generateGPT(trimmedText);
+
+    if (!response) {
+      ctx.reply(
+        "Sorry, I couldn't generate content for you. You ran out of the free API credits. Please try again.",
+      );
+    } else {
+      const formattedResponse = `<b style="color:green;">Voila 🪄 Your greeting is ready!</b>\n\n${response.content}`;
+      ctx.replyWithHTML(
+        formattedResponse,
+        Markup.inlineKeyboard([
+          Markup.button.callback(
+            "👎 I don't like it. Give me another one!",
+            'generate_new',
+          ),
+        ]),
+      );
+    }
+  } catch (error) {
+    console.log(`Error with OpenAI processing:`, error.message);
+  }
+}
 
 bot.action('generate_new', async (ctx) => {
-    const userId = ctx.from.id;
-    // track to many regenerate on the bad keyword input
-    if (!buttonClickCount[userId]) {
-        buttonClickCount[userId] = 0;
-    }
+  const userId = ctx.from.id;
+  // track excessive button clicks to regenerate on the bad keyword input
+  if (!buttonClickCount[userId]) {
+    buttonClickCount[userId] = 0;
+  }
 
-    const tipsMessage =
-        `<b style="color:green;">Sorry, I'm struggling 😓 to come up with a greeting you like</b>\n
+  const tipsMessage = `<b style="color:green;">Sorry, I'm struggling 😓 to come up with a greeting you like</b>\n
     <b>💡Tip:</b> \nTry to be specific with your keywords. Tell me <b> who </b> is it for, and what's the <b> occasion </b> (e.g. birthday, new home,get better)`;
-    if (buttonClickCount[userId] >= 1) {
-        ctx.replyWithHTML(tipsMessage);
-        return;
-    }
+  if (buttonClickCount[userId] >= 1) {
+    ctx.replyWithHTML(tipsMessage);
+    return;
+  }
 
-    buttonClickCount[userId]++;
-    const originalInput = originalUserInput[userId];
+  buttonClickCount[userId]++;
+  const originalInput = originalUserInput[userId];
 
-    ctx.answerCbQuery('Working on a new greeting...');
-    try {
-        // Re-generate a new response using the original user input
-        const newResponse = await generateGPT(originalInput);
-        ctx.answerCbQuery('Generating a new description...');
+  ctx.answerCbQuery('Working on a new greeting...');
+  try {
+    // Re-generate a new response using the original user input
+    const newResponse = await generateGPT(originalInput);
+    ctx.answerCbQuery('Generating a new description...');
 
-        const formattedNewResponse = `<b>Ok! What about this one? </b>\n\n${newResponse.content}`;
-        ctx.replyWithHTML(formattedNewResponse, Markup.inlineKeyboard([
-            Markup.button.callback("👎 I don't like it. Give me another one!", 'generate_new')
-        ]));
-    } catch (error) {
-        console.error('Error with re-generation', error.message);
-    }
+    const formattedNewResponse = `<b>Ok! What about this one? </b>\n\n${newResponse.content}`;
+    ctx.replyWithHTML(
+      formattedNewResponse,
+      Markup.inlineKeyboard([
+        Markup.button.callback(
+          "👎 I don't like it. Give me another one!",
+          'generate_new',
+        ),
+      ]),
+    );
+  } catch (error) {
+    console.error('Error with re-generation', error.message);
+  }
 });
 
-
-bot.launch()
+bot.launch();
--- src/openai.js
+++ src/openai.js
@@ -1,36 +1,33 @@
-import OpenAI from "openai";
-import config from 'config'
+import OpenAI from 'openai';
+import config from 'config';
 
-const GPT_MODEL = 'gpt-3.5-turbo'
-const ROLES = { USER: 'user', ASSISTANT: 'assistant', SYSTEM: 'system' }
+const GPT_MODEL = 'gpt-3.5-turbo-0301';
+const ROLES = { USER: 'user', ASSISTANT: 'assistant', SYSTEM: 'system' };
 const openai = new OpenAI({
-    apiKey: config.get('APIKeys.OPENAI_TOKEN'),
+  apiKey: config.get('APIKeys.OPENAI_TOKEN'),
 });
 
-
-const getUserKeywords = (keywords) => `Write 160 charachrters long text which is based on the provided keywords: ${keywords}.`
+const getUserKeywords = (keywords) =>
+  `Write 100 characters long text which is based on the provided keywords: ${keywords}.`;
 
 export async function generateGPT(message = '') {
-    const messages = [
-        {
-            role: ROLES.SYSTEM,
-            content: config.get('Prompts.CONTENT_PROMPT'),
-        },
-        {
-            role: ROLES.USER,
-            content: getUserKeywords(message),
-        }
-    ];
-    try {
-        const chatCompletion = await openai.chat.completions.create({
-            messages,
-            model: GPT_MODEL,
-        });
-        return chatCompletion.choices[0].message
-    } catch (error) {
-        console.error('Error while chat completion', error);
-
-    }
-
+  const messages = [
+    {
+      role: ROLES.SYSTEM,
+      content: config.get('Prompts.CONTENT_PROMPT'),
+    },
+    {
+      role: ROLES.USER,
+      content: getUserKeywords(message),
+    },
+  ];
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      messages,
+      model: GPT_MODEL,
+    });
+    return chatCompletion.choices[0].message;
+  } catch (error) {
+    console.error('Error while chat completion', error);
+  }
 }
-
